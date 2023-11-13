c13caf601e760e584b5d9f1d069af80db4eaa53d
fguerrero2 <fguerrero2@gmail.com>
2023-11-13 17:25:53 -0300

    agregamos comentarios

--- js/setearcolores.js
+++ js/setearcolores.js
@@ -1,9 +1,16 @@
+/*obtenemos el elemento a asignar un evento*/ 
+
 const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
-            function cambiaTema(ev){
-                if(ev.target.checked){
-                    document.documentElement.setAttribute('tema', 'dark');
-                } else {
-                    document.documentElement.setAttribute('tema', 'light');
-                }
-            }
-            colorSwitch.addEventListener('change', cambiaTema);
+
+/*asignamos al evento change (modificacion) nuestra funcion */
+colorSwitch.addEventListener('change', cambiaTema);
+
+/* funcion que setea el atributo tema segun la opcion del checkbox*/
+function cambiaTema(ev){
+    if(ev.target.checked){
+        document.documentElement.setAttribute('tema', 'dark');
+    } else {
+        document.documentElement.setAttribute('tema', 'light');
+    }
+}
+
